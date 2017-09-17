462057e9a75d8335fc668441c2d6a6fb2de420ba
iLampard <siqiao_xue@163.com>
2017-09-17 22:32:12 +0800

    Fix recursion

--- 分治和递归/recursion.cpp
+++ 分治和递归/recursion.cpp
@@ -2,8 +2,37 @@
 //  recursion.cpp
 //  algo
 //
-//  Created by Siqiao on 2017/9/17.
-//  Copyright © 2017年 Siqiao. All rights reserved.
+//  Created on 2017/9/17.
 //
 
-#include "recursion.hpp"
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+// 围棋棋盘由M*N的正方形组成，计算这个区域一共由多少个正方形
+int numOfRectangular(int M, int N)
+{
+    std::vector<std::vector<int> > go(M, std::vector<int>(N, 0));
+    int i,j;
+    int count = 0;
+    for(i = 1; i < M; i++)
+    {
+        for(j = 1; j < N; j++)
+        {
+            if( i != j)
+                go[i][j] = std::max(go[i][j-1], go[i-1][j]);
+            else
+                go[i][j] = go[i-1][j] + 1;
+            count += go[i][j];
+        }
+    }
+    return count;
+}
+
+
+
+int main()
+{
+    std::cout<<"19*19的围棋棋盘包含的正方形数目为......"<<std::endl;
+    std::cout<<numOfRectangular(19, 19)<<std::endl;
+}
