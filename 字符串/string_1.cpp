947844e88997cac170bf7ad1efd7069a0f41b7bf
iLampard <siqiao_xue@163.com>
2017-09-17 23:22:56 +0800

    overwrite old string file

--- 字符串/string_1.cpp
+++ 字符串/string_1.cpp
@@ -2,8 +2,40 @@
 //  string_1.cpp
 //  algo
 //
-//  Created by Siqiao on 2017/9/17.
-//  Copyright © 2017年 Siqiao. All rights reserved.
+//  Created on 2017/9/17.
 //
 
-#include "string_1.hpp"
+#include "iostream"
+
+// 字符串循环左移： 把字符串的前k个字符平移到末尾
+// 时间复杂度O(N), 空间复杂度O(1)
+
+void reverseString(char* s, int from, int to)
+{
+    while(from < to)
+    {
+        char temp = s[from];
+        s[from++] = s[to];
+        s[to--] = temp;
+    }
+}
+
+void leftRotateString(char* s, int iLen, int iRotate)
+{
+    int m = iRotate % iLen;
+    reverseString(s, 0, m-1);
+    reverseString(s, m, iLen-1);
+    reverseString(s, 0, iLen-1);
+}
+
+
+int main()
+{
+    std::cout<<"Left rotate string with size 2...."<<std::endl;
+    std::cout<<"input: abcdef"<<std::endl;
+    char s[] = "abcdef";
+    leftRotateString(s, 6, 2);
+    std::cout<<"output:"<<s<<std::endl;
+    
+}
+
