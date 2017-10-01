ad1ea1b0502924a95c8b01e38c43c83c3bfd10a1
iLampard <siqiao_xue@163.com>
2017-10-01 16:21:56 +0800

    fix manacher algo

--- 字符串/manacher.cpp
+++ 字符串/manacher.cpp
@@ -9,7 +9,7 @@
 
 
 
-char* conver_string(char* s)
+char* convert_string(char* s)
 {
     size_t iLen = strlen(s);
     char* str_convert = new char[2 * iLen + 1];
@@ -31,31 +31,51 @@ int* manacher(char* s)
     P[0] = 1;
     int i;
     int mx = 1;
+    int id = 1;
     for(i = 1; i < iLen; i++)
     {
         if(i >= mx)
         {
             P[i] = 1;
-            for(;s[i - P[i]] == s[i + P[i]]; P[i]++);
         }
         else
         {
-            
+            P[i] = std::min(P[2*id-i], mx-i);
+        }
+        
+        for(;s[i - P[i]] == s[i + P[i]]; P[i]++);
+        
+        if(i + P[i] > mx)
+        {
+            mx = i + P[i];
+            id = i;
         }
     }
     
     return P;
 }
 
-
+int findMaxLength(int* P, int iLen)
+{
+    int max = P[0];
+    for(int i = 1; i < iLen; i++)
+    {
+        max = P[i] > max ? P[i]: max;
+    }
+    return max;
+}
 
 
 int main()
 {
-    char s[] = "12345654312";
+    char s[] = "12212321";
     std::cout<<"converted string is "<<std::endl;
-    std::cout<<conver_string(s)<<std::endl;
+    std::cout<<convert_string(s)<<std::endl;
     int* P = manacher(s);
     std::cout<<"max length of is  "<<std::endl;
-    std::cout<<conver_string(s)<<std::endl;
+    //std::cout<<convert_string(s)<<std::endl;
+    char* convert_str = convert_string(s);
+    P = manacher(convert_str);
+    std::cout<<findMaxLength(P, (int)strlen(convert_str))/2<< std::endl;
+    
 }
