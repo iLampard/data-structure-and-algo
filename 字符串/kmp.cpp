8aebec2a630c9da1d80bfc9ca6037634268e8c57
iLampard <siqiao_xue@163.com>
2017-10-01 17:09:35 +0800

    add kmp algo example

--- 字符串/kmp.cpp
+++ 字符串/kmp.cpp
@@ -2,8 +2,46 @@
 //  kmp.cpp
 //  algo
 //
-//  Created by Siqiao on 2017/10/1.
-//  Copyright © 2017年 Siqiao. All rights reserved.
+//  Created on 2017/10/1.
+//  All rights reserved.
 //
 
-#include "kmp.hpp"
+#include <iostream>
+
+
+// 查找text中首次出现pattern的位置
+
+
+int bruteForceSearch(char* text, char* pattern)
+{
+    int i = 0;  // index for text
+    int j;  // index for pattern
+    size_t iLenText = strlen(text);
+    size_t iLenPattern = strlen(pattern);
+    
+    while(i + iLenPattern < iLenText)
+    {
+        j = 0;
+        while(text[i+j] == pattern[j])
+        {
+            j++;
+        }
+        if(j == iLenPattern)
+            return i;
+        else
+            i++;
+    }
+    
+    return -1;
+}
+
+
+int main(void)
+{
+    char text[] = "abcdefg";
+    char pattern[] = "cde";
+    std::cout<<"find pattern in text:...."<<std::endl;
+    std::cout<<"brute force method gives start index"<<std::endl;
+    std::cout<<bruteForceSearch(text, pattern)<<std::endl;
+    return 0;
+}
