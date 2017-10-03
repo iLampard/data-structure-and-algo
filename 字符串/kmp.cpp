7ac3408aafdefa076be7c7e3540ca95c851dfe00
iLampard <siqiao_xue@163.com>
2017-10-03 23:14:59 +0800

    add kmp algo

--- 字符串/kmp.cpp
+++ 字符串/kmp.cpp
@@ -36,12 +36,85 @@ int bruteForceSearch(char* text, char* pattern)
 }
 
 
+
+// KMP
+
+// caculate "next" array
+
+int* calcNext(char* pattern)
+{
+    size_t iLen = strlen(pattern);
+    int* next = new int[iLen];
+    next[0] = -1;
+    int j = 1;
+    int k = -1;;
+    while(j < iLen - 1)
+    {
+        // k即是next[j-1], p[k]表示前缀， p[j]表示后缀
+        if(k == - 1 || pattern[k] == pattern[j])
+        {
+            next[j+1] = k + 1;
+            k++;
+            j++;
+        }
+        else
+        {
+            k = next[k];
+        }
+    }
+    
+    return next;
+}
+
+
+void printArray(int* array, int iLen)
+{
+    for(int i = 0; i < iLen; i++)
+        std::cout<<array[i]<<' ';
+    std::cout<<std::endl;
+}
+
+
+int KMP(char* text, char* pattern)
+{
+    int i = 0;  // index for text
+    int j = 0;  // index for pattern
+    size_t iLenText = strlen(text);
+    size_t iLenPattern = strlen(pattern);
+    
+    while(i < iLenText)
+    {
+        if(j == -1 || text[i] == pattern[j])
+        {
+            i++;
+            j++;
+        }
+        else
+        {
+            j = calcNext(pattern)[j];
+        }
+        if(j == iLenPattern)
+            return (i - (int)iLenPattern);
+        
+    }
+    
+    return -1;
+}
+
 int main(void)
 {
-    char text[] = "abcdefg";
-    char pattern[] = "cde";
+    char text[] = "abcabaabcabafg";
+    char pattern[] = "abaabcaba";
     std::cout<<"find pattern in text:...."<<std::endl;
     std::cout<<"brute force method gives start index"<<std::endl;
     std::cout<<bruteForceSearch(text, pattern)<<std::endl;
+    
+    //int* next;
+    //next = calcNext(pattern);
+    //printArray(next, (int)strlen(pattern));
+    std::cout<<"KMP method gives start index"<<std::endl;
+    std::cout<<KMP(text, pattern)<<std::endl;
+    
+    
     return 0;
 }
