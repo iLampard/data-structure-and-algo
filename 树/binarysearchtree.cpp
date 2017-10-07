250cfc8058aeb8c4110668243293296a69f0372b
iLampard <siqiao_xue@163.com>
2017-10-07 23:27:40 +0800

    add more examples in main function

--- 树/binarysearchtree.cpp
+++ 树/binarysearchtree.cpp
@@ -6,7 +6,8 @@
 //  All rights reserved.
 //
 
-#include <stdio.h>
+#include <algorithm>
+#include <iostream>
 
 
 typedef struct TreeNode *Position;
@@ -34,14 +35,30 @@ SearchTree Delete(ElementType X, SearchTree T);
 void InOrderTraverse(SearchTree T, VISIT visit);
 void PreOrderTraverse(SearchTree T, VISIT visit);
 void PostOrderTraverse(SearchTree T, VISIT visit);
-void InPre2PostOrder(char* pInOrder, char* pPreOrder, char** pPostOrder, int* iPostOrder);
-void InPost2PreOrder(char* pInOrder, char* pPostOrder, int iLen, char** pPreOrder, int* iPreOrder)
-bool FeasiblePostOrder(int* pPostOrder, int iLen)
+void InPre2PostOrder(char* pInOrder, char* pPreOrder, int iLen, char** pPostOrder, int* iPostOrder);
+void InPost2PreOrder(char* pInOrder, char* pPostOrder, int iLen, char** pPreOrder, int* iPreOrder);
+bool FeasiblePostOrder(int* pPostOrder, int iLen);
+bool LargestBinarySearchTree(SearchTree T,   // 输入的整棵二叉树
+                             int* iMinElement, // 二叉树T的最小结点值
+                             int* iMaxElement,  // 二叉树T的最大结点值 
+                             int* iNbNode,   // 二叉搜索树的结点数总和
+                             int* iMaxNbNode, // 保存的最大二叉搜索树的结点数总和 
+                             Position* pNode);  // 二叉搜索树的根结点
+void Reverse(SearchTree* T);
+SearchTree CreateTree(ElementType* array, int iLen);
+void PrintElement(ElementType X);
 
+void TraverseExample();
+void InPre2PostOrderExample();
+void InPost2PreOrderExample();
 
 
 int main()
 {
+    TraverseExample();
+    InPre2PostOrderExample();
+    InPost2PreOrderExample();
+    
     return 0;
 }
 
@@ -105,9 +122,9 @@ SearchTree Insert(ElementType X, SearchTree T)
         T = new TreeNode(X);
     }
     if(X > T->Element)
-        return Insert(X, T->Right);  // 插入到右子树中
+        T->Right = Insert(X, T->Right);  // 插入到右子树中
     else if (X < T->Element)
-        return Insert(X, T->Left);   // 插入到左子树中
+        T->Left = Insert(X, T->Left);   // 插入到左子树中
     /* 如果X已经在树中，则什么都不用做 */
     return T;
 }
@@ -186,14 +203,11 @@ void PostOrderTraverse(SearchTree T, VISIT visit)
 void InPre2PostOrder(char* pInOrder, char* pPreOrder, int iLen, char** pPostOrder, int* iPostOrder)
 {
     if(iLen == 0)
-    {
-        (*pPostOrder) = NULL;
         return;
-    }
         
     if(iLen == 1)  // 如果前序和中序只有一个字符，那么说明该字符属于根结点
     {
-        (*pPostOrder)[0] = *pPreOrder; // 此种情况下， 前序和中序等价
+        (*pPostOrder)[(*iPostOrder)] = *pPreOrder; // 此种情况下， 前序和中序等价
         (*iPostOrder)++;
         return;
     }
@@ -209,10 +223,10 @@ void InPre2PostOrder(char* pInOrder, char* pPreOrder, int iLen, char** pPostOrde
     // 中序 => 左子树中序 [pInOrder, pInOrder+i-1], 右子树[pInOrder+i+1, pInOrder+iLen]
     // 前序 => 左子树前序 [pPreOrder+1, pPreOrder+i], 右子树[pPreOrder+i+1, pPreOrder+iLen]
     InPre2PostOrder(pInOrder, pPreOrder+1, i, pPostOrder, iPostOrder); 
-    InPre2PostOrder(pInOrder+i+1, pPreOrder+i+1, iLen-i-1, pPorstOrder, iPostOrder)；
+    InPre2PostOrder(pInOrder+i+1, pPreOrder+i+1, iLen-i-1, pPostOrder, iPostOrder);
     // 给根结点赋值
-    (*pPostOrder)[iPostOrder] = root;
-    iPostOrder++; 
+    (*pPostOrder)[(*iPostOrder)] = root;
+    (*iPostOrder)++;
 }
 
 
@@ -221,14 +235,11 @@ void InPre2PostOrder(char* pInOrder, char* pPreOrder, int iLen, char** pPostOrde
 void InPost2PreOrder(char* pInOrder, char* pPostOrder, int iLen, char** pPreOrder, int* iPreOrder)
 {
     if(iLen == 0)
-    {
-        (*pPreOrder) = NULL;
         return;
-    }
         
     if(iLen == 1)  // 如果中序和后序只有一个字符，那么说明该字符属于根结点
     {
-        (*pPreOrder)[0] = *pPostOrder; // 此种情况下， 中序和后序等价
+        (*pPreOrder)[(*iPreOrder)] = *pPostOrder; // 此种情况下， 中序和后序等价
         (*iPreOrder)++;
         return;
     }
@@ -242,13 +253,13 @@ void InPost2PreOrder(char* pInOrder, char* pPostOrder, int iLen, char** pPreOrde
     }
     
     // 给根结点赋值
-    (*pPreOrder)[iPreOrder] = root;
-    iPreOrder++;
+    (*pPreOrder)[(*iPreOrder)] = root;
+    (*iPreOrder)++;
     // 对左右子树分别递归
     // 中序 => 左子树中序 [pInOrder, pInOrder+i-1], 右子树[pInOrder+i+1, pInOrder+iLen]
     // 后序 => 左子树前序 [pPostOrder, pPostOrder+i-1], 右子树[pPostOrder+i, pPostOrder+iLen-1]
-    InPre2PostOrder(pInOrder, pPostOrder, i, pPreOrder, iPreOrder); 
-    InPre2PostOrder(pInOrder+i+1, pPostOrder+i, iLen-i-1, pPreOrder, iPreOrder)；
+    InPost2PreOrder(pInOrder, pPostOrder, i, pPreOrder, iPreOrder); 
+    InPost2PreOrder(pInOrder+i+1, pPostOrder+i, iLen-i-1, pPreOrder, iPreOrder);
 
 }
 
@@ -281,5 +292,130 @@ bool FeasiblePostOrder(int* pPostOrder, int iLen)
     && FeasiblePostOrder(pPostOrder+i, iLen-i-1); // 右子树
 }
 
+
+/* 找到一颗二叉树里面的最大二叉搜索树 */
+// 规定： 如果某子树拥有更多的结点，则该子树更大
+// 算法： 如果某结点的左右子树均是二叉搜索树，且能够计算该结点左子树的最大值max和右子树的最小值，集该结点的值为value
+//       若value > max && value < min, 则该结点形成了更大的二叉搜索树
+bool LargestBinarySearchTree(SearchTree T,   // 输入的整棵二叉树
+                             int* iMinElement, // 二叉树T的最小结点值
+                             int* iMaxElement,  // 二叉树T的最大结点值 
+                             int* iNbNode,   // 二叉搜索树的结点数总和
+                             int* iMaxNbNode, // 保存的最大二叉搜索树的结点数总和 
+                             Position* pNode)  // 二叉搜索树的根结点
+{
+    (*iNbNode) = 0;
+    if(!T)
+        return true;
+    int iMaxLeft_ = INT_MIN, iMinLeft_ = INT_MAX, iMaxRight_ = INT_MIN, iMinRight_ = INT_MAX;
+    int iNbNodeLeft_, iNbNodeRight_;
+    // 递归： 对左子树进行搜索 - 获取左子树节点的最小值，最大值，结点数，最大搜索树二叉树的结点数， 最大二叉搜索树的根结点
+    if(!LargestBinarySearchTree(T->Left, &iMinLeft_, &iMaxLeft_, &iNbNodeLeft_, iMaxNbNode, pNode))
+        return false;
+    // 递归： 对右子树进行搜索 - 同上
+    if(!LargestBinarySearchTree(T->Right, &iMinRight_, &iMaxRight_, &iNbNodeRight_, iMaxNbNode, pNode))
+        return false;
+
+    // 如果当前树的根结点小于左子树的最大值 或者 大于右子树的最小值 => 当前树T不是二叉搜索树
+    if(T->Element < iMaxLeft_ || T->Element > iMinRight_)
+        return false;
+
+    // 总的结点数 = 左右子树的结点数 + 1 
+    (*iNbNode) = iNbNodeLeft_ + iNbNodeRight_ + 1;  
+
+    (*iMinElement) = std::min(iMinLeft_, T->Element);  // 当前树的最小值更新
+    (*iMaxElement) = std::max(iMaxRight_, T->Element); // 当前树的最大值更新
+
+    if((*iMaxNbNode) < (*iNbNode)) // 至此当前树为二叉搜索树， 如果其结点数目比保存的最大结点数还要大
+    {
+        (*iMaxNbNode) = (*iNbNode);
+        (*pNode) = T;
+    }
+
+    return true;
+}
+
+
+/* 二叉树的翻转 */
+// 算法： 交换左右孩子的指针，然后递归考察左右子树即可
+void Reverse(SearchTree *T)
+{
+    
+    if(!T)
+    {
+        std::swap((*T)->Left, (*T)->Right);
+        Reverse(&(*T)->Left);
+        Reverse(&(*T)->Right);
+    }
+}
+
+
+/* 给定一个数组，创建一个搜索二叉树 */
+SearchTree CreateTree(ElementType* array, int iLen)
+{
+    SearchTree T = NULL;
+    for(int i = 0 ; i < iLen; i++)
+    {
+        T = Insert(array[i], T);
+    }
+    return T;
+}
+
+
+/* 打印结点值 */
+void PrintElement(ElementType X)
+{
+    std::cout<<X<<'\t';
+}
+
+
+/* 创建一个前中后遍历的例子  */
+void TraverseExample()
+{
+    std::cout<<"示例：前中后遍历"<<std::endl;
+    SearchTree T = NULL;
+    int element[] = {15, 5, 3, 12, 16, 20, 23, 13, 18, 10, 6, 7};
+    int iLen = sizeof(element)/sizeof(int);
+    /* 创建一棵二叉搜索树，并且尝试中序，前序以及后序遍历（打印）*/
+    T = CreateTree(element, iLen);
+    std::cout<<"中序遍历序列为"<<std::endl;
+    InOrderTraverse(T, PrintElement);
+    std::cout<<"前序遍历序列为"<<std::endl;
+    PreOrderTraverse(T, PrintElement);
+    std::cout<<"后序遍历序列为"<<std::endl;
+    PostOrderTraverse(T, PrintElement);
+    
+}
+
+
+/* 创建一个已知前序中序，求后序序列的例子 */
+void InPre2PostOrderExample()
+{
+    std::cout<<"示例：已知前序中序，求后序序列"<<std::endl;
+    char pPreOrder[] = "GDAFEMHZ";
+    char pInOrder[] = "ADEFGHMZ";
+    int iLen = sizeof(pInOrder)/sizeof(char);
+    char* pPostOrder = new char[iLen];
+    int iPostOrder = 0;
+    InPre2PostOrder(pInOrder, pPreOrder, iLen-1, &pPostOrder, &iPostOrder);
+    pPostOrder[iLen-1] = 0;
+    std::cout<<pPostOrder<<std::endl;
+    delete[] pPostOrder;
+}
+
+/* 创建一个已知前序中序，求后序序列的例子 */
+void InPost2PreOrderExample()
+{
+    std::cout<<"示例：已知中序后序，求前序序列"<<std::endl;
+    char pPostOrder[] = "AEFDHZMG";
+    char pInOrder[] = "ADEFGHMZ";
+    int iLen = sizeof(pInOrder)/sizeof(char);
+    char* pPreOrder = new char[iLen];
+    int iPreOrder = 0;
+    InPost2PreOrder(pInOrder, pPostOrder, iLen-1, &pPreOrder, &iPreOrder);
+    pPreOrder[iLen-1] = 0;
+    std::cout<<pPreOrder<<std::endl;
+    delete[] pPreOrder;
+}
 
 
