c9c3c8e9b70420531b779353b8a512ab0e2ce84c
iLampard <siqiao_xue@163.com>
2017-10-11 23:08:14 +0800

    update array

--- 数组/array.cpp
+++ 数组/array.cpp
@@ -1,8 +1,10 @@
 
 
 
-# include <iostream>
+#include <iostream>
 
+
+／*  二分法搜索     *／
 int binarySearch(int* array, int iLen, int iTarget)
 {
 	int iBegin = 0;
@@ -26,3 +28,35 @@ int binarySearch(int* array, int iLen, int iTarget)
 	}
 	return -1;
 }
+
+
+
+/* 已知给定的N个整数存在绝对众数，以最低的时空复杂度计算该绝对众数  */
+/* 众数出现次数大于N/2 => 绝对众数   */
+// 算法：删除数组中两个不同的数，绝对众数不变；依据此原则遍历数组
+int AbsMode(int* array, int iLen)
+{
+	int iCount = 0;
+	int i;
+	int mode;
+	for(; i < iLen; i++)
+	{
+		if(!iCount) // iCount == 0, 则 mode = array[i]
+		{
+			mode = array[i];
+			iCount = 1;
+		}	
+		else if(mode != array[i]) // array[i]与众数不相等时，二者都删除 <=> iCount减少1， 跳过array[i]
+		{
+			iCount --;
+		}
+		else  // array[i]与众数相等时
+		{
+			iCount++;
+		}
+
+	}
+	return mode;
+}
+
+
