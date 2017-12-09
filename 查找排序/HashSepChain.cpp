ee7d4b44d933b4c882fcafaaa575f09051212d3a
iLampard <siqiao_xue@163.com>
2017-12-10 00:39:12 +0800

    Add init hash table function(incomplete)

--- 查找排序/HashSepChain.cpp
+++ 查找排序/HashSepChain.cpp
@@ -4,7 +4,7 @@
 
 
 typedef struct ListNode *Position;
-typedef Position *List;
+typedef Position List;
 typedef struct HashTbl *HashTable;
 typedef int ElementType;
 
@@ -17,5 +17,26 @@ struct ListNode
 struct HashTbl
 {
 	int TableSize;
-	List *TheLists;
+	List *TheLists;  // 指向ListNode结构的指针的指针
 };
+
+/* 求接下来的一个素数 */
+int NextPrime(int n);
+
+/* 初始化HashTable */
+HashTable InitTable(int TableSize);
+
+
+int main()
+{
+	return 0;
+}
+
+
+HashTable InitTable(int TalbeSize)
+{
+	int i;
+	HashTable H = new HashTbl;
+	H->TalbeSize = NextPrime(TableSize);
+
+}
