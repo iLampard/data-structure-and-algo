10befb3590e571a93747e5b058737a1590eec0a6
iLampard <162405siqiao>
2018-01-02 14:49:27 +0800

    fix bugs in adjacencylist

--- 图/AdjacencyList.cpp
+++ 图/AdjacencyList.cpp
@@ -11,7 +11,7 @@ typedef struct VertexNode AdjList[MAX_VERTEX];
 typedef struct GraphAdjList *List;
 
 
-enum GraphClass {Digraph，UnDigraph};
+enum GraphClass {Digraph, UnDigraph};
 
 /* 边的表节点 */
 struct EdgeNode 
@@ -37,30 +37,29 @@ struct GraphAdjList
 	int NumVertex, NumEdge; /* 图的顶底数和边数 */
 };
 
+
 /* 输入一列顶点 以及边的信息，返回链接表 */
 List CreateGraph(Vertex* pVertex,     // 顶点列表
 				 int NumVertex,
-				 Vertex* pEdgeStart,  // 边的起始顶点列表
-				 Vertex* pEdgeEnd,    // 边的终点顶点列表
+				 Vertex ppEdge[][2], // 二维数组，每一行代表一条边的起始定点, 2列（暂不考虑权重）
 				 int NumEdge,
-				 GraphClass Class)
+				 GraphClass GraphClassKind);
 
 
 void PrintArray(Vertex* array, int size);
 
-void PrintEdge(Vertex* pEdgeStart, Vertex* pEdgeEnd, int NumEdge);
+void PrintEdge(Vertex** ppEdge, int NumEdge);
 
-
-
+/* 有向图的例子 */
 void DigraphExample();
 
-
+/* 无向图的例子 */
+void UnDigraphExample();
 
 int main()
 {
-
-	int NumVertex = 5;
-	int NumEdge = 6;
+	DigraphExample();
+	UnDigraphExample();
 	return 0;
 }
 
@@ -74,36 +73,43 @@ void PrintArray(Vertex* array, int size)
 }
 
 
-void PrintEdge(Vertex* pEdgeStart, Vertex* pEdgeEnd, int NumEdge)
+
+void PrintEdge(Vertex ppEdge[][2], int NumEdge, GraphClass GraphClassKind=UnDigraph)
 {
 	for(int i = 0; i < NumEdge; i++)
-		std::cout<<pEdgeStart[i]<<" - "<<pEdgeEnd[i]<<std::endl;
+		if (GraphClassKind == UnDigraph)
+			std::cout<<ppEdge[i][0]<<" - "<<ppEdge[i][1]<<std::endl;
+		else
+			std::cout<<ppEdge[i][0]<<" -> "<<ppEdge[i][1]<<std::endl;
 	std::cout<<std::endl;
 }
 
 
-void PrintGraphAdjList(List L)
+void PrintGraphAdjList(List L, GraphClass GraphClassKind=UnDigraph)
 {
 	for(int i = 0; i < L->NumVertex; i++)
 	{
-		
+		std::cout<<L->Header[i].Data<<" ";
+		Position P = L->Header[i].FirstEdge;
+		while(P)
+		{	
+			if (GraphClassKind == UnDigraph)
+				std::cout<<" -- "<<P->AdjVex<<" ";
+			else
+				std::cout<<" --> "<<P->AdjVex<<" ";
+			P = P->Next;
+		}
+		std::cout<<std::endl;
 	}
 }
 
 
 List CreateGraph(Vertex* pVertex,
 				 int NumVertex,
-				 Vertex* pEdgeStart,
-				 Vertex* pEdgeEnd,
+				 Vertex ppEdge[][2],
 				 int NumEdge,
-				 GraphClass Class)
+				 GraphClass GraphClassKind)
 {
-	std::cout<<"Vertex is "<<std::endl;
-	PrintArray(pVertex, NumVertex);
-
-	std::cout<<"Edge is "<<std::endl;
-	PrintEdge(pEdgeStart, pEdgeEnd, NumEdge);
-
 	int i;
 	List L = new GraphAdjList;
 	L->NumVertex = NumVertex;
@@ -118,8 +124,8 @@ List CreateGraph(Vertex* pVertex,
 
 	for(i = 0; i < L->NumEdge; i++)
 	{
-		start = pEdgeStart[i];
-		end = pEdgeEnd[i];
+		Vertex start = ppEdge[i][0];
+		Vertex end = ppEdge[i][1];
 		
 		/* P1的指针指向当前顶点指向的节点 */
 		Position P1 = new EdgeNode;
@@ -127,7 +133,7 @@ List CreateGraph(Vertex* pVertex,
 		P1->Next = L->Header[start].FirstEdge;
 		L->Header[start].FirstEdge = P1;
 
-		if(Class == UnDigraph) // 如果是无向图的话，边的尾节点也要加入到对应的邻接矩阵中去
+		if(GraphClassKind == UnDigraph) // 如果是无向图的话，边的尾节点也要加入到对应的邻接矩阵中去
 		{
 			Position P2 = new EdgeNode;
 			P2->AdjVex = start;
@@ -136,18 +142,54 @@ List CreateGraph(Vertex* pVertex,
 		}
 
 	}
-
+	return L;
 }
 
 
 
 void DigraphExample()
 {
-	GraphClass Class = Digraph;
+	std::cout<<"An example of digraph..... "<<std::endl;
+
+	GraphClass GraphClassKind = Digraph;
 	Vertex pVertex[] = {0, 1, 2, 3};
-	NumVertex = sizeof(pVertex) / sizeof(Vertex);
-	Vertex pStart[] = {0, 0, 2, 3};
-	Vertex pEnd[] = {1, 2, 3, 0};
-	NumEdge = sizeof(pStart) / sizeof(Vertex);
-	List L = CreateGraph(pVertex, NumVertex, pStart, pEnd, NumEdge);
+	int NumVertex = sizeof(pVertex) / sizeof(Vertex);
+	Vertex ppEdge[][2] = {{0, 1}, {0, 2}, {2, 3}, {3, 0}};
+	int NumEdge = sizeof(ppEdge) / sizeof(ppEdge[0]);
+
+	std::cout<<"Vertex is "<<std::endl;
+	PrintArray(pVertex, NumVertex);
+
+	std::cout<<"Edge is "<<std::endl;
+	PrintEdge(ppEdge, NumEdge, GraphClassKind);
+
+	List L = CreateGraph(pVertex, NumVertex, ppEdge, NumEdge, GraphClassKind);
+
+	std::cout<<"Adjacency List is "<<std::endl;
+	PrintGraphAdjList(L, GraphClassKind);
+	system("pause");
+}
+
+
+void UnDigraphExample()
+{
+	std::cout<<"An example of undigraph..... "<<std::endl;
+
+	GraphClass GraphClassKind = UnDigraph;
+	Vertex pVertex[] = {0, 1, 2, 3};
+	int NumVertex = sizeof(pVertex) / sizeof(Vertex);
+	Vertex ppEdge[][2] = {{0, 1}, {0, 2}, {2, 3}, {3, 0}};
+	int NumEdge = sizeof(ppEdge) / sizeof(ppEdge[0]);
+
+	std::cout<<"Vertex is "<<std::endl;
+	PrintArray(pVertex, NumVertex);
+
+	std::cout<<"Edge is "<<std::endl;
+	PrintEdge(ppEdge, NumEdge, GraphClassKind);
+
+	List L = CreateGraph(pVertex, NumVertex, ppEdge, NumEdge, GraphClassKind);
+
+	std::cout<<"Adjacency List is "<<std::endl;
+	PrintGraphAdjList(L, GraphClassKind);
+	system("pause");
 }
