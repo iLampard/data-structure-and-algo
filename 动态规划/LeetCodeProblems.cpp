a8a104cc0499488f9b60766e080a930787dab7a3
iLampard <siqiao_xue@163.com>
2018-01-25 22:56:19 +0800

    Add a climb_stair problem

--- 动态规划/LeetCodeProblems.cpp
+++ 动态规划/LeetCodeProblems.cpp
@@ -6,6 +6,11 @@
 int SolveHourseRobber(int idx, int*HouseValues, int* Result);
 int HouseRobber(int* HouseValues, int NumHouse);
 
+
+// https://leetcode.com/problems/climbing-stairs/description/
+int ClimbStairs(int n);
+
+
 int main()
 {
     int HouseValues[] = {1, 2, 7, 1, 10};
@@ -13,6 +18,8 @@ int main()
     std::cout<<"Max House Rob Values is \n" << HouseRobber(HouseValues, NumHouse)<<std::endl;
     return 0;
 }
+
+
 
 
 int SolveHourseRobber(int idx, int*HouseValues, int* Result)
@@ -29,10 +36,29 @@ int SolveHourseRobber(int idx, int*HouseValues, int* Result)
     return Result[idx];
 }
 
+
 int HouseRobber(int* HouseValues, int NumHouse)
 {
     int* Result = new int [NumHouse];
     for(int i = 0; i < NumHouse; i++)
         Result[i] = -1;
     return SolveHourseRobber(NumHouse-1, HouseValues, Result);
+}
+
+
+
+// https://leetcode.com/problems/climbing-stairs/description/
+int ClimbStairs(int n)
+{
+    int dp[3] = {1, 1};
+    if(n < 2)
+        return 1;  // 如果只有一个台阶， 那就只有一种跳法
+    for(int i = 2; i <= n; i++)
+    {
+        dp[2] = dp[1] + dp[0];
+        dp[0] = dp[1];
+        dp[1] = dp[2];
+    }
+
+    return dp[2];
 }
